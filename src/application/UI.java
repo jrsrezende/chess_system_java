08898a8e4129330bb41f4a67c1d48feafe3e2108
João Rezende <joaoricardorezende@gmail.com>
2025-06-05 18:59:38 -0300

    Switch player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
 import Chess.Color;
@@ -44,7 +45,14 @@ public class UI {
         }
     }
 
-    public static void printBoard(ChessPiece[][] pieces) {
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+    }
+
+    private static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces[i].length; j++) {
