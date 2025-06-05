cb515651c4dfa32434b3baae819347e72a390752
João Rezende <joaoricardorezende@gmail.com>
2025-06-05 15:14:28 -0300

    Possible moves of a piece

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 @echo off
-cd /d "%~dp0out\production\chess_system_java"
-java application.Program
+cd /d "%~dp0"
+java -cp out\production\chess_system_java application.Program
+
+
--- src/BoardGame/Piece.java
+++ src/BoardGame/Piece.java
@@ -1,6 +1,6 @@
 package BoardGame;
 
-public class Piece {
+public abstract class Piece {
 
     protected Position position; // não tem valor nenhum dentro da matriz pieces[][](tabuleiro), só serve de referencia
     private Board board;
@@ -11,5 +11,23 @@ public class Piece {
 
     protected Board getBoard(){
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possiblemove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[i].length; j++){
+                if (mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
